9d9b5060e4bdbb8078a8bd6ca2b4e5547b1e9d1c
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-01-31 12:38:20 -0300

    chore(app): improve styling

--- main.js
+++ main.js
@@ -2,15 +2,73 @@ const form = document.querySelector('form');
 const button = document.querySelector('#liview-button');
 const input = document.querySelector('#url-input');
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-
+const isValidURL = (url) => {
   try {
-    window.open(input.value, '_self');
-  } catch (err) {
-    console.error(err);
+    new URL(url);
+  } catch {
+    return false;
   }
+
+  const re =
+    // eslint-disable-next-line max-len
+    /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+
+  return re.test(url);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const url = input.value;
+
+  if (!isValidURL(url)) {
+    return;
+  }
+
+  window.open(url, '_self');
 };
 
 form.addEventListener('submit', handleSubmit);
 button.addEventListener('click', handleSubmit);
+
+button.addEventListener('mouseover', () => {
+  const url = input.value;
+
+  if (!url) {
+    button.style.cursor = 'not-allowed';
+  }
+});
+
+const changeParentBorderColor = (element, color) => {
+  element.parentElement.style.border = `2px solid ${color}`;
+};
+
+const changeParentShadowColor = (element, color) => {
+  element.parentElement.style.boxShadow = `0 10px 15px -3px ${color}`;
+};
+
+input.addEventListener('focus', () => {
+  changeParentBorderColor(input, '#95959c');
+  changeParentShadowColor(input, '#ffffff0d');
+
+  const url = input.value;
+
+  if (!url) {
+    input.value = 'http://';
+  }
+});
+
+input.addEventListener('focusout', () => {
+  const url = input.value;
+
+  if (!isValidURL(url) && url !== 'http://' && url !== '') {
+    changeParentBorderColor(input, '#cc312a');
+    changeParentShadowColor(input, '#cc312a4d');
+  } else if (isValidURL(url) && url !== 'http://' && url !== '') {
+    changeParentBorderColor(input, '#22c55e');
+    changeParentShadowColor(input, '#22c55e4d');
+  } else {
+    changeParentBorderColor(input, '#646465');
+    changeParentShadowColor(input, '#ffffff0d');
+  }
+});
